a9b086e803b2eac1cbb282454e6a3a317981e0c0
User <gicagarcia58@gmail.com>
2024-06-10 22:40:43 -0300

    refactor: changing driver to Safari

--- src/test/java/org/example/Base.java
+++ src/test/java/org/example/Base.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
@@ -10,8 +11,8 @@ public abstract class Base {
     protected WebDriverWait wait;
     protected Faker faker;
 
-    public Base(WebDriver driver) {
-        this.driver = driver;
+    public Base(SafariDriver driver) {
+        this.driver = new SafariDriver();
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         this.faker = new Faker();
     }
