4b20ad5f944977425b65ec56dc13d9e704067ea6
guizinsx <guizin51@gmail.com>
2024-06-11 13:39:30 -0300

    feat: update HomePage to handle iframes for navigation links

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,43 +3,59 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
     private final WebDriver driver;
+    private final WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public WebElement getTransportLink() {
-        return driver.findElement(By.xpath("//a[@href='transporte/transporte.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='transporte/transporte.html']"));
     }
 
     public WebElement getVehicleLink() {
-        return driver.findElement(By.xpath("//a[@href='veiculos/veiculos.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='veiculos/veiculos.html']"));
     }
 
     public WebElement getMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/mercadorias.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/mercadorias.html']"));
     }
 
     public WebElement getInsertMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/mercadorias.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/mercadorias.html']"));
     }
 
     public WebElement getUpdateMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/alterar.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/alterar.html']"));
     }
 
     public WebElement getDeleteMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/excluir.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/excluir.html']"));
     }
 
     public WebElement getListAllMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/listartodos.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/listartodos.html']"));
     }
 
     public WebElement getListOneMerchandiseLink() {
-        return driver.findElement(By.xpath("//a[@href='mercadorias/listarum.html']"));
+        return getLinkInHeader(By.xpath("//a[@href='mercadorias/listarum.html']"));
+    }
+
+    private WebElement getLinkInHeader(By by) {
+        WebElement headerIframe = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@src='header.html']")));
+        driver.switchTo().frame(headerIframe);
+
+        WebElement link = wait.until(ExpectedConditions.elementToBeClickable(by));
+        driver.switchTo().defaultContent();
+
+        return link;
     }
 }
