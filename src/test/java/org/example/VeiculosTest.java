2c8f158b668c8ac97e42d6169aa140610fcc2ca8
User <gicagarcia58@gmail.com>
2024-06-11 19:01:48 -0300

    feat: setup on VeiculosTest to start testing page

--- src/test/java/org/example/VeiculosTest.java
+++ src/test/java/org/example/VeiculosTest.java
@@ -1,2 +1,39 @@
-package org.example;public class VeiculosTest {
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VeiculosTest {
+    private WebDriver driver;
+    private VeiculosPage veiculosPage;
+    protected WebDriverWait wait;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.safaridriver().setup();
+        driver = new SafariDriver();
+        driver.manage().window().maximize();
+        driver.get("https://site-vercel-tc1.vercel.app/veiculos/veiculos.html");
+
+        veiculosPage = new VeiculosPage(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
