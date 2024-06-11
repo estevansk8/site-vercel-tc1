718a60e1c2ce741cf94e914c3f505ec992b14e9c
User <gicagarcia58@gmail.com>
2024-06-10 22:29:39 -0300

    feat: created fake data to insert vehicle

--- src/test/java/org/example/VeiculosPage.java
+++ src/test/java/org/example/VeiculosPage.java
@@ -1,4 +1,27 @@
-package org.example.pages;
+package org.example;
 
-public class VeiculosPage {
+import org.openqa.selenium.WebDriver;
+
+import java.util.Random;
+
+public class VeiculosPage extends Base{
+
+    public VeiculosPage(WebDriver driver) {
+        super(driver);
+    }
+
+    Random random = new Random();
+    int anoInicio = 1900;
+    int anoFim = 2024;
+
+    String plate = faker.bothify("???-####");
+    String city = faker.address().city();
+    String state = faker.address().state();
+    String make = faker.lorem().word();
+    String model = faker.lorem().word();
+    int year = anoInicio + random.nextInt(anoFim - anoInicio + 1);
+    String color = faker.color().name();
+    String speed = faker.bothify("###");
+
+
 }
