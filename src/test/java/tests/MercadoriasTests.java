a8d4d9bb04b795cf40b05d82d2291c0fb26b6f1c
guizinsx <guizin51@gmail.com>
2024-06-11 01:46:03 -0300

    fix: ensure correct option for fragility dropdown and alert message to match portuguese text switch context to iframe

--- src/test/java/tests/MercadoriasTests.java
+++ src/test/java/tests/MercadoriasTests.java
@@ -1,11 +1,23 @@
 package tests;
 
-import pages.HomePage;
-import pages.MercadoriasPage;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.HomePage;
+import pages.MercadoriasPage;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +27,15 @@ public class MercadoriasTests extends TesteBase {
     @DisplayName("Should add a new merchandise")
     void shouldAddNewMerchandise() throws InterruptedException {
         HomePage homePage = new HomePage(driver);
-        homePage.getMerchandiseLink().click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement headerIframe = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@src='header.html']")));
+        driver.switchTo().frame(headerIframe);
+
+        WebElement merchandiseLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='mercadorias/mercadorias.html']")));
+        merchandiseLink.click();
+
+        driver.switchTo().defaultContent();
 
         MercadoriasPage mercadoriasPage = new MercadoriasPage(driver);
         mercadoriasPage.getCodeField().sendKeys(faker.number().digits(6));
@@ -25,16 +45,18 @@ public class MercadoriasTests extends TesteBase {
         mercadoriasPage.getHeightField().sendKeys("100");
         mercadoriasPage.getWidthField().sendKeys("50");
         mercadoriasPage.getVolumeField().sendKeys("5000");
-        mercadoriasPage.getFragilityDropdown().selectByVisibleText("Fragil");
+
+        WebElement fragilityDropdown = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("fragilidade")));
+        Select selectFragility = new Select(fragilityDropdown);
+        selectFragility.selectByVisibleText("Fragíl");
+
         mercadoriasPage.getInsertButton().click();
 
         wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
         String alertText = alert.getText();
-        String expectedText = "Merchandise added successfully";
+        String expectedText = "Cadastrado com sucesso!";
         assertThat(alertText).isEqualTo(expectedText);
         alert.accept();
     }
-
-
 }
