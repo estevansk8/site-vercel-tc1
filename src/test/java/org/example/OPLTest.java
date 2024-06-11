1f808433ccb912860eaf2810ddf304aa8f181594
otaviopl <otavio.pereira.lopes@gmail.com>
2024-06-11 09:35:03 -0300

    ADD: add some neww tests for driver ... commit to continue at work

--- src/test/java/org/example/OPLTest.java
+++ src/test/java/org/example/OPLTest.java
@@ -1,27 +1,51 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OPLTest {
     private WebDriver driver;
+    private WebDriverWait wait;
+    private Faker faker;
+    private final String BASE_URL = "https://site-vercel-tc1.vercel.app/mercadorias/mercadorias.html";
 
     @BeforeEach
     public void setUp() {
-        initializeWebDriver();
-        navigateToBaseUrl();
+        System.out.println("Setting up driver");
+
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver/chrome-linux64/chrome");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
     }
 
     @AfterEach
     public void tearDown() {
-        closeWebDriver();
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    @Test
+    @DisplayName("Should open and close chrome browser using Manager")
+    void shouldOpenAndCloseChromeBrowserUsingManager() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        driver.quit();
     }
 
     @Test
@@ -31,20 +55,10 @@ public class OPLTest {
         verifySuccessMessage();
     }
 
-    private void initializeWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        driver = new ChromeDriver();
-    }
-
-    private void navigateToBaseUrl() {
-        String BASE_URL = "https://site-vercel-tc1.vercel.app/mercadorias/mercadorias.html";
-        driver.get(BASE_URL);
-    }
-
-    private void closeWebDriver() {
-        if (driver != null) {
-            driver.quit();
-        }
+    @Test
+    public void testGetSiteTitle() {
+        String title = getSiteTitle();
+        assertEquals("Speed", title); // Substitua "Expected Title" pelo título esperado do site
     }
 
     private void fillInFormWithTestData() {
@@ -71,5 +85,10 @@ public class OPLTest {
     private void verifySuccessMessage() {
         WebElement successMessage = driver.findElement(By.id("resultado"));
         assertEquals("Mercadoria inserida com sucesso!", successMessage.getText());
+    }
+
+    private String getSiteTitle() {
+        driver.get(BASE_URL);
+        return driver.getTitle();
     }
 }
